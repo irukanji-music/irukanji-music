b85288e988a122d0b197698ce61705cfd2a18da8
Ihor Orlovskyi <ihor.droid@gmail.com>
2015-06-29 00:50:53 +0300

    add menu js

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,19 +112,19 @@ scroller.init();
 
 
 // Header menu FX
-var $head = $( '.nav-top' );
+var $head = $( 'header' );
 $('.header-waypoint').each( function(i) {
 	var $el = $( this ),
 		animClassDown = $el.data( 'animateDown' ),
 		animClassUp = $el.data( 'animateUp' );
 	$el.waypoint( function( direction ) {
 		if( direction === 'down' && animClassDown ) {
-			$head.attr('class', 'nav-main nav-top ' + animClassDown);
+			$head.attr('class', 'header ' + animClassDown);
 		}
 		else if( direction === 'up' && animClassUp ){
-			$head.attr('class', 'nav-main nav-top ' + animClassUp);
+			$head.attr('class', 'header ' + animClassUp);
 		}
-	}, { offset: '-25%' } );
+	}, { offset: '-1px' } );
 });
 
 
