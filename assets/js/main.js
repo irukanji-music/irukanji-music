968d448646892cd0963ec30125eb90a6e56b39e4
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-01-19 16:40:59 +0200

    huge cleaning

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,111 +1,4 @@
 /**
- * Created by Sallar Kaboli <sallar.kaboli@gmail.com>
- * @sallar
- * 
- * Released under the MIT License.
- * http://sallar.mit-license.org/
- * 
- * This document demonstrates three things:
- * 
- * - Creating a simple parallax effect on the content
- * - Creating a Medium.com-style blur on scroll image
- * - Getting scroll position using requestAnimationFrame for better performance
- */
-
-
-/**
- * Cache
- */
-var $content = $('.index-head .content')
-	, $blur    = $('.index-head .overlay')
-	, wHeight  = $(window).height();
-
-$(window).on('resize', function(){
-	wHeight = $(window).height();
-});
-
-/**
- * requestAnimationFrame Shim 
- */
-window.requestAnimFrame = (function()
-{
-	return  window.requestAnimationFrame       ||
-					window.webkitRequestAnimationFrame ||
-					window.mozRequestAnimationFrame    ||
-					function( callback ){
-						window.setTimeout(callback, 1000 / 60);
-					};
-})();
-
-/**
- * Scroller
- */
-function Scroller()
-{
-	this.latestKnownScrollY = 0;
-	this.ticking            = false;
-}
-
-Scroller.prototype = {
-	/**
-	 * Initialize
-	 */
-	init: function() {
-		window.addEventListener('scroll', this.onScroll.bind(this), false);
-	},
-
-	/**
-	 * Capture Scroll
-	 */
-	onScroll: function() {
-		this.latestKnownScrollY = window.scrollY;
-		this.requestTick();
-	},
-
-	/**
-	 * Request a Tick
-	 */
-	requestTick: function() {
-		if( !this.ticking ) {
-			window.requestAnimFrame(this.update.bind(this));
-		}
-		this.ticking = true;
-	},
-
-	/**
-	 * Update.
-	 */
-	update: function() {
-		var currentScrollY = this.latestKnownScrollY;
-		this.ticking       = false;
-		
-		/**
-		 * Do The Dirty Work Here
-		 */
-		var slowScroll = currentScrollY / 4,
-				blurScroll = currentScrollY * 2;
-		
-		$content.css({
-			'transform'         : 'translateY(-' + slowScroll + 'px)',
-			'-moz-transform'    : 'translateY(-' + slowScroll + 'px)',
-			'-webkit-transform' : 'translateY(-' + slowScroll + 'px)'
-		});
-		
-		$blur.css({
-			'opacity' : blurScroll / wHeight
-		});
-	}
-};
-
-/**
- * Attach!
- */
-var scroller = new Scroller();  
-scroller.init();
-
-
-
-/**
 * waypoint
 * https://github.com/imakewebthings/waypoints
 */
@@ -126,33 +19,10 @@ $('.header-waypoint').each( function(i) {
 
 
 /**
-* isotope with imagesloaded 
-* https://github.com/metafizzy/isotope
-* https://github.com/desandro/imagesloaded
-*/
-var $container = $('.isotope-container');
-$(document).ready(function() {
-	$container.imagesLoaded( function() {
-		$container.isotope({
-			itemSelector: '.isotope-item',
-			layoutMode: 'fitRows',
-		});
-	});
-});
-
-
-
-/**
 * fittext
 * https://github.com/davatron5000/FitText.js
 */
-// $('.index-head hgroup h1').fitText(0.75);
-// $('.index-head hgroup h2').fitText(3.75);
 $('.parallax-window h1').fitText(1.50);
-
-$('.index-releases .title').fitText(1.25);
-$('.index-releases .description').fitText(2.00);
-
 // $('.release-title').fitText(1.25);
 // $('.release-description').fitText(2.00);
 
