559ee074a7511f962e0c944874e16bf98aec170d
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-08-12 23:59:28 +0300

    add gulpfile.js, package.json, _config_dev.yml

---
+++ _pages/home.md
@@ -0,0 +1,12 @@
+---
+layout: default
+permalink: /home/
+---
+
+# Home
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti recusandae, quo soluta inventore sit nemo libero omnis nobis, architecto vero, amet quaerat necessitatibus dolorem aspernatur doloremque. Numquam quos, fuga ullam. Eveniet enim facilis, aspernatur cupiditate. Dolorum cumque, modi laudantium dicta praesentium incidunt sed soluta provident, expedita culpa facere laborum blanditiis libero, ea enim odio accusantium unde consequuntur nobis nulla nihil neque eligendi debitis tenetur natus! Eveniet?
+
+## Lorem ipsum dolor sit.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sunt corporis accusantium quas eligendi officia fugiat. Voluptas tenetur repellendus deleniti, incidunt at. Quod facilis qui culpa esse, vero unde sit et distinctio inventore alias magni rerum maiores nihil voluptates veritatis minima eum accusamus aliquam illum dolore iste labore similique. Doloribus ducimus ipsa laboriosam sed molestiae. Nihil omnis veritatis sequi maiores quia ut minus sit voluptas commodi deleniti facilis molestiae nemo aperiam odit id doloribus quibusdam sapiente quos, accusamus eveniet nulla alias quo! Ab, quo, ipsam. Accusantium sequi adipisci libero aut quam voluptatum nisi fugit cum sunt ex. Fugiat maiores magni iusto numquam necessitatibus illum voluptate excepturi! Perferendis repellendus iste, tempore accusantium ducimus! Hic nam commodi, dolore velit quae, voluptatibus facere neque asperiores unde at expedita in repellendus? Omnis, aliquam. Soluta, fugiat, dicta.
--- gulpfile.js
+++ gulpfile.js
@@ -2,35 +2,30 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync'),
     sass = require('gulp-sass'),
     prefix = require('gulp-autoprefixer'),
-    cp = require('child_process'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
     gutil = require('gulp-util'),
     plumber = require('gulp-plumber'),
-    spritesmith = require('gulp.spritesmith'),
-    iconfont = require('gulp-iconfont'),
-    iconfontCss = require('gulp-iconfont-css'),
-    sassUnicode = require('gulp-sass-unicode');
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
 
 var messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+    jekyllBuild: 'gulp jekyll-build'
 };
 
-/* Build the Jekyll Site */
+// gulp jekyll-build
 gulp.task('jekyll-build', ['jsMin'], function (done) {
     browserSync.notify(messages.jekyllBuild);
     var jekyll = process.platform === "win32" ? "jekyll.bat" : "jekyll";
-    return cp.spawn(jekyll, ['build'], {stdio: 'inherit'})
+    return cp.spawn(jekyll, ['build', '--config', '_config.yml,_config_dev.yml'], {stdio: 'inherit'})
         .on('close', done);
 });
 
-/* Rebuild Jekyll & do page reload */
+// gulp jekyll-rebuild
 gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-    return browserSync.reload();
+    browserSync.reload();
 });
 
-/* Wait for jekyll-build, then launch the Server */
-gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+// gulp browser-sync
+gulp.task('browser-sync', ['sass'], function() {
     browserSync({
         server: {
             baseDir: '_site'
@@ -38,13 +33,10 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
     });
 });
 
-/* Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds) */
+// gulp sass
 gulp.task('sass', function () {
     return gulp.src([
-            '_scss/index.scss',
-            '_scss/vu5.scss',
-            '_scss/lotoru.scss',
-            '_scss/ssl.scss'
+            '_scss/main.scss'
         ])
         .pipe(plumber(function(error) {
             gutil.log(gutil.colors.red(error.message));
@@ -52,39 +44,38 @@ gulp.task('sass', function () {
         }))
         .pipe(sass({
             includePaths: ['scss'],
-            outputStyle: 'expanded',
-            onError: browserSync.notify
+            outputStyle: 'compressed'
         }))
-        .pipe(sassUnicode())
         .pipe(prefix(['last 2 versions', 'ie 8', 'ie 9'], { cascade: true }))
-        .pipe(gulp.dest('_site/assets/css'))
+        .pipe(gulp.dest('_site/assets/css/'))
         .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('assets/css'));
+        .pipe(gulp.dest('assets/css/'));
 });
 
-/* Js */
-gulp.task('jsConcat', function() {
+// gulp js-concat
+gulp.task('js-concat', function() {
     return gulp.src([
             '_js/lib/*',
             '_js/vendor/*',
-            '_components/**/*.js'
+            '_components/**/*.js',
         ])
         .pipe(concat('all.min.js'))
-        .pipe(gulp.dest('assets/js'));
+        .pipe(gulp.dest('assets/js/'));
 });
 
-gulp.task('jsMin', ['jsConcat'], function() {
+// gulp js-min
+gulp.task('js-min', ['js-concat'], function() {
     return gulp.src(['assets/js/all.min.js'])
         .pipe(plumber(function(error) {
             gutil.log(gutil.colors.red(error.message));
             this.emit('end');
         }))
         .pipe(uglify())
-        .pipe(gulp.dest('assets/js'));
+        .pipe(gulp.dest('assets/js/'));
 });
 
-/* Watch scss files for changes & recompile, watch html/md files, run jekyll & reload BrowserSync */
-gulp.task('watch', ['jsMin', 'browser-sync'], function () {
+// gulp watch
+gulp.task('watch', ['js-min', 'browser-sync'], function () {
     gulp.watch([
         '_scss/**/*.scss',
         '_components/**/*.scss',
@@ -96,53 +87,11 @@ gulp.task('watch', ['jsMin', 'browser-sync'], function () {
         '_components/**/*.yml',
         '_components/**/*.json',
         '_pages/**/*',
-        '_posts/**/*',
         'assets/img/**/*',
         'assets/fonts/**/*',
         '_config.yml'
     ], ['jekyll-rebuild']);
 });
 
-/* Default task, running just `gulp` will compile the sass, compile the jekyll site, launch BrowserSync & watch files. */
+// gulp
 gulp.task('default', ['sass', 'jekyll-build']);
-
-/* Payments Sprite */
-gulp.task('sprite', function () {
-    var spriteData = gulp.src([
-        'assets/img/payments/*.png',
-        'assets/img/languages/*.png'
-    ]).pipe(spritesmith({
-        imgName: 'sprite.png',
-        imgPath: '../img/sprites/sprite.png',
-        cssName: '../../../_scss/vendor/_sprite.scss',
-        padding: 12
-    }));
-    return spriteData.pipe(gulp.dest('assets/img/sprites/'));
-});
-
-/* svg fonts task */
-
-gulp.task('fonts', function () {
-    var fontName = 'svgFont';
-
-    var projectName = process.argv.slice(3).toString().replace('--', '');
-    var opt = {
-        src: 'assets/svg/' + projectName + '/*.svg',
-        path: '_scss/vendor/fontIconsTemplate/_icons.scss',
-        targetPath: '../../../_scss/vendor/_' + projectName + '-icons.scss',
-        fontPath: '../fonts/' + projectName +'/',
-        dest: 'assets/fonts/' + projectName
-    };
-
-    return gulp.src([opt.src])
-        .pipe(iconfontCss({
-            fontName: fontName,
-            path: opt.path,
-            targetPath: opt.targetPath,
-            fontPath: opt.fontPath,
-        })).pipe(iconfont({
-            fontName: fontName,
-            formats: ['svg', 'ttf', 'eot', 'woff', 'woff2'],
-            normalize: true
-        })).pipe(gulp.dest(opt.dest));
-});
